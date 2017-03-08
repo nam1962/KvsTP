e5881a189a1f4ea9508d607d8e42e4ce835a0787
nam1962 <amauryat@e-nautia.com>
2017-03-08 14:43:41 +0100

    Create KvsTP.sh

---
+++ KvsTP.sh
@@ -0,0 +1,1 @@
+printf '[Desktop Entry]\n Name=Keystroke-vs-Touchpad\n Exec=syndaemon -i 1.0 -d -K\n Encoding=UTF-8\n Type=Application\n X-GNOME-Autostart-enabled=true\n OnlyShowIn=XFCE;\n StartupNotify=false\n Terminal=false\n Hidden=false\n Comment=disables touchpad while typing\n GenericName=Keystroke vs Touchpad' > ~/.config/autostart/keystoke-vs-touchpad.desktop
